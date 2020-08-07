67b8a0741d7861ab5906e65044b3e6ad20417940
João Vítor Souza Pioner <joaopioner123@gmail.com>
2020-08-07 13:31:10 -0300

    Adicionei um exercício.

---
+++ ExMedia-Sequencial.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    
+    double nota1, nota2, media;
+    
+    cout << "Insira a primeira nota do aluno: ";
+    cin >> nota1;
+    cout << "Insira a segunda nota do aluno: ";
+    cin >> nota2;
+    
+    media = (nota1 + nota2) / 2;
+    
+    cout << "A media das notas do aluno é: " << media << endl;
+    
+}
+
+
+
