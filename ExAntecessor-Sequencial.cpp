965018f9a6e84698afeb92df59b6df7306da4362
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-08-15 23:28:20 -0300

    Adicionei o exercício antecessor sequencial.

---
+++ ExAntecessor-Sequencial.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int input;
+    
+    cout << "Insira um numero inteiro: ";
+    cin >> input;
+    
+    cout << "O antecessor do numero = " << input - 1 << endl;
+    
+    return 0;
+}
