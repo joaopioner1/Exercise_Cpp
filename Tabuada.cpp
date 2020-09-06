7ab7063fa7ffa1208f729f844dcd090222c8a819
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-09-06 20:46:49 -0300

    Update Tabuada.cpp

--- Tabuada.cpp
+++ Tabuada.cpp
@@ -7,7 +7,6 @@ int main()
     int input;
     
     cin >> input;
-    
     for (int i = 1; i <= 10; i++) {
         int mul = i * input;
         cout << i << " x " << input << " = " << mul << endl;
