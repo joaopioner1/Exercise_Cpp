51d1146cfa25e8b59d24ef417b58245b44bd09e3
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-09-11 11:44:30 -0300

    Adc o programa media notas.

---
+++ MediaNotas.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    cout << "CALCULANDO_MEDIA" << endl;
+    cout << "Digite a quantidade de notas que deseja inserir: ";
+
+    return 0;
+}
