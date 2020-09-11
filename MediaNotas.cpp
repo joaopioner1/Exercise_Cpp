fba50eab21947dcda99322129dfeae331218becc
Joao Vitor Pioner <joaopioner123@gmail.com>
2020-09-11 11:56:51 -0300

    Update MediaNotas.cpp

--- MediaNotas.cpp
+++ MediaNotas.cpp
@@ -4,8 +4,17 @@ using namespace std;
 
 int main()
 {
+    int n;
+    
     cout << "CALCULANDO_MEDIA" << endl;
     cout << "Digite a quantidade de notas que deseja inserir: ";
+    cin >> n;
+    
+    for (int i = 0; i < n; i++) {
+        cout << "Insira uma nota: ";
+        cin >> nota;
+        
+    }
 
     return 0;
 }
